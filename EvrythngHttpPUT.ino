d8c64e3a11eccc93554523d8777dad77f28fa02b
stevend1974 <stevend1974ch@gmail.com>
2014-01-27 10:14:19 +0100

    Added Apache Licence
    Added Apache Licence in all files
    corrected endless loop in http put file

--- EvrythngHttpPUT.ino
+++ EvrythngHttpPUT.ino
@@ -1,4 +1,22 @@
 /*
+   Copyright 2014 Steven D.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+   
+ */
+
+
+/*
   Sketch Demonstrating updating a thng property on the Evrythng platform
   
   Variables to modify
@@ -22,20 +40,32 @@
       - if the HTTP response is OK (200) -> get properties of thng
       
   output on the serial port will be a simile of
-
-
   
+  connecting...
+  connected... 
+       HTTP PUT      
+        Object posted with length 47 :      [{"key": "property1","value": "updatedvalue1"}]
+       HTTP PUT completed 
+    http response stream.... 
+      checking out the response
+      HTTP/1.1 result code is :<200>
+   testing finished -> sketch stops working
   
-  Steven D. version 1.0
  */
+
+
+
 #include <SPI.h>
 #include <Ethernet.h>
   
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 char serverName[] = "api.evrythng.com";
-const char *APIToken = "replace_by_your_evrythng_api_token";
-const char *thngid="replace_by_your_evrythng_thng_id";
+//const char *APIToken = "replace_by_your_evrythng_api_token";
+//const char *thngid="replace_by_your_evrythng_thng_id";
+const char *APIToken = "Oz28gK1LijEoxWJKblXoHWGGrnbMaPUpjqAMnKRx2GoSFikMwig1Zcrb94GlZhiRBTtzYSWIeOTFzovI";
+const char *thngid="52e411f7e4b0a7fd3090b31a";
+
 
 boolean endtest;
 
@@ -45,8 +75,6 @@ EthernetClient client;
 
 char propertystr[] = "[{\"key\": \"property1\",\"value\": \"updatedvalue1\"}]";
 
-
-boolean gethttpresponsecode;
 
 int result; // the result of the calculation
 
@@ -61,7 +89,6 @@ void setup()
   delay(1000); // give the Ethernet shield a second to initialize
 
   endtest = false;
-  gethttpresponsecode=true;
 
   Serial.println("connecting...");
 }
@@ -69,13 +96,14 @@ void setup()
 void loop()
 {
   if (client.connect(serverName, 80)>0) {
-    Serial.print("connected... ");
+    Serial.println("connected... ");
     
               
     // Make a HTTP request PUT:                            
                             
-    Serial.println ("     HTTP POST ");
-    Serial.println ("      Object posted with length "+String(strlen(propertystr))+" : ");
+    Serial.println ("     HTTP PUT ");
+    Serial.println ("     ");
+    Serial.print ("      Object posted with length "+String(strlen(propertystr))+" : ");
     Serial.print("     ");Serial.println (propertystr);
     Serial.println (" ");
               
@@ -99,39 +127,26 @@ void loop()
   }
   if (client.connected()) {
     // check for http response code
-    Serial.println("http response stream.... ");
-    
-    if (gethttpresponsecode){
-    
-    if(client.find("HTTP/1.1 ")){
-        Serial.println("  checking out the response");
-               result = client.parseInt();
-               Serial.print("HTTP/1.1 result code is :<" );
-               Serial.print(result);
-               Serial.println(">"); 
-               
-               if (result!=200){
-                 // http response code is NOT OK  (200) so we stop
-                 endtest = true;
-                 Serial.println (" http response is not ok...stopping test");
-                 
-                 }
-               // we are not looking anymore for the httpresponse code
-               gethttpresponsecode = false;
-   }
+    Serial.println("  http response stream.... ");
+   
+   
+          if(client.find("HTTP/1.1 ")){
+              Serial.println("    checking out the response");
+                     result = client.parseInt();
+                     Serial.print("    HTTP/1.1 result code is :<" );
+                     Serial.print(result);
+                     Serial.println(">"); 
+                     
+                     if (result!=200){
+                       // http response code is NOT OK  (200) so we stop
+                       endtest = true;
+                       Serial.println ("  http response is not ok...stopping test");
+                       
+                       }
 
-  } else { // no need for httpresponse code just the properties
-     if(client.find ("properties\":{")){
-        Serial.println("  checking out properties");
-       
-        propertiesstring = client.readStringUntil('}}') ; 
-        Serial.println (" ---> "+propertiesstring);
-        propertiesstring="";
-        endtest = true;
-       }
-     
-  } // end not just httpresponse code
-       
+         } 
+         
+    endtest = true;   
     client.stop();
     client.flush();
     delay(10000); // check again in 10 seconds
